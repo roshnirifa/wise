f3bb65c16393031dc0c740acf7a9951254a87e2a
roshnirifa <roshnirifa998@gmail.com>
2022-09-11 22:23:55 +0600

    navbar added

---
+++ src/Pages/Home/Header/HeaderItems/HeaderItems.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+const HeaderItems = () => {
+    return (
+        <div class="navbar bg-base-100 container">
+            <div class="navbar-start">
+                <div class="dropdown">
+
+                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a>Home</a></li>
+                        <li tabindex="0">
+                            <a class="justify-between">
+                                Books
+                                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
+                            </a>
+                            <ul class="p-2">
+                                <li><a>books 1</a></li>
+                                <li><a>books 2</a></li>
+                            </ul>
+                        </li>
+
+                        <li tabindex="0">
+                            <a class="justify-between">
+                                Courses
+                                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
+                            </a>
+                            <ul class="p-2">
+                                <li><a>Courses 1</a></li>
+                                <li><a>Courses 2</a></li>
+                            </ul>
+                        </li>
+
+
+                        <li><a>Instructors</a></li>
+                        <li><a>Blogs</a></li>
+                        <li><a>Contact</a></li>
+                    </ul>
+                </div>
+
+            </div>
+            <div class="navbar-center hidden lg:flex">
+                <ul class="menu menu-horizontal p-0">
+                    <li><a>Home</a></li>
+                    <li tabindex="0">
+                        <a>
+                            Books
+                            <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+                        </a>
+                        <ul class="p-2">
+                            <li><a>books 1</a></li>
+                            <li><a>books 2</a></li>
+                        </ul>
+                    </li>
+
+                    <li tabindex="0">
+                        <a>
+                            Courses
+                            <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+                        </a>
+                        <ul class="p-2">
+                            <li><a>Courses 1</a></li>
+                            <li><a>Courses 2</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a>Instructors</a></li>
+                    <li><a>Blogs</a></li>
+                    <li><a>Contact</a></li>
+                </ul>
+            </div>
+            <div class="navbar-end">
+                <a class="btn">Get in Touch</a>
+            </div>
+        </div>
+    );
+};
+
+export default HeaderItems;
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header/Header';
+import HeaderItems from './Header/HeaderItems/HeaderItems';
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <HeaderItems></HeaderItems>
+        </div>
+    );
+};
+
+export default Home;
