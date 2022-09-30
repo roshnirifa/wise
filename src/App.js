9caecdf75c1f67d46d4acc5eedf0cf3b4f74dab7
roshnirifa <roshnirifa998@gmail.com>
2022-09-30 22:55:10 +0600

    user profile added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import NotFound from './Pages/NotFound/NotFound';
 import Login from './Pages/Home/Login/Login';
 
 import Register from './Pages/Home/Register/Register';
+import UserProfile from './Pages/UserProfile/UserProfile';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
@@ -18,8 +20,11 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='/userProfile' element={<UserProfile></UserProfile>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
+
       </Routes>
+      <ToastContainer></ToastContainer>
     </div>
   );
 }
