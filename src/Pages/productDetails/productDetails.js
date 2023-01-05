7129981c3cf7dae611d9373e2f173300312bd262
maahidhasan <mahidsabbir900@gmail.com>
2023-01-05 21:07:09 +0600

    pDetails 1

---
+++ src/Pages/productDetails/productDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const productDetails = () => {
+    return (
+        <div>
+            {/* <ReactLoading type={type} color={color} height={'20%'} width={'20%'} /> */}
+        </div>
+    );
+};
+
+export default productDetails;
