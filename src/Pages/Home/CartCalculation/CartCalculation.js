77ca0805ae785ddf1e4a7d6a650cdaec9be48927
jubairahmed22 <jubairahmed060@gmail.com>
2023-01-05 21:55:13 +0600

    cart calculations syntex added

---
+++ src/Pages/Home/CartCalculation/CartCalculation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CartCalculation = () => {
+    return (
+        <div>
+            <h1>This is calculations</h1>
+        </div>
+    );
+};
+
+export default CartCalculation;
