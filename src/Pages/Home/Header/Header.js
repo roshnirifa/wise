4cf5c6afa2633c4e09128b74f89b7cfeee2b6a1c
roshnirifa <roshnirifa998@gmail.com>
2023-02-01 11:15:30 +0600

    cart

--- src/Pages/Home/Header/Header.js
+++ src/Pages/Home/Header/Header.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { signOut } from 'firebase/auth';
 import { Link } from 'react-router-dom';
-import logo from '../../../img/logo.png'
+import logo from '../../../img/logo.png';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../firebaseInit';
 import UseBooksOnSale from '../../UseBooksOnSale/UseBooksOnSale';
@@ -13,12 +13,12 @@ const Header = () => {
     const [user] = useAuthState(auth);
 
     const [cart, setCart] = useState(initialCart)
-    const handleRemoveItem = (id) => {
-        const remaining = cart.filter(product => product.id !== id)
-        console.log(remaining);
-        setCart(remaining);
-        removeFromDb(id)
-    }
+    // const handleRemoveItem = (id) => {
+    //     const remaining = cart.filter(product => product.id !== id)
+    //     console.log(remaining);
+    //     setCart(remaining);
+    //     removeFromDb(id)
+    // }
 
 
 
@@ -44,10 +44,16 @@ const Header = () => {
 
                     <div class="dropdown dropdown-end">
                         <label tabindex="0" class="btn btn-ghost btn-circle mr-5">
-                            <div class="indicator">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                                <span class="badge badge-sm indicator-item">{initialCart.length}</span>
-                            </div>
+
+                            {
+                                user && <div class="indicator">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                                    <span class="badge badge-sm indicator-item">{initialCart.length}</span>
+                                </div>
+                            }
+
+
+
                         </label>
 
                         <div tabindex="0" class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow ">
